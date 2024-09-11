db0abc82dc24449c394fd02466aa6733753219e9
meyason <nori.tobimori@icloud.com>
2024-09-12 01:24:01 +0900

    Shinku kawaii

--- src/main/java/space/yurisi/universecorev2/subplugins/mywarp/menu/mywarp_menu/MywarpInventoryMenu.java
+++ src/main/java/space/yurisi/universecorev2/subplugins/mywarp/menu/mywarp_menu/MywarpInventoryMenu.java
@@ -4,12 +4,7 @@ import org.bukkit.*;
 import org.bukkit.entity.Player;
 import space.yurisi.universecorev2.menu.BaseMenu;
 import space.yurisi.universecorev2.subplugins.mywarp.connector.UniverseCoreAPIConnector;
-import space.yurisi.universecorev2.subplugins.mywarp.menu.del_menu.item.DelMenuItem;
-import space.yurisi.universecorev2.subplugins.mywarp.menu.mywarp_menu.item.DelMywarpMenuItem;
-import space.yurisi.universecorev2.subplugins.mywarp.menu.mywarp_menu.item.ListMywarpMenuItem;
-import space.yurisi.universecorev2.subplugins.mywarp.menu.mywarp_menu.item.AddMywarpMenuItem;
-import space.yurisi.universecorev2.subplugins.mywarp.menu.mywarp_menu.item.SettingMywarpMenuItem;
-import space.yurisi.universecorev2.subplugins.mywarp.menu.setting_menu.SettingMywarpInventoryMenu;
+import space.yurisi.universecorev2.subplugins.mywarp.menu.mywarp_menu.item.*;
 import xyz.xenondevs.invui.gui.Gui;
 import xyz.xenondevs.invui.item.Item;
 import xyz.xenondevs.invui.item.builder.ItemBuilder;
@@ -29,7 +24,7 @@ public class MywarpInventoryMenu implements BaseMenu {
         Gui gui = Gui.normal()
                 .setStructure(
                         "# # # # # # # # #",
-                        "# a b c d . . . #",
+                        "# a b c d e . . #",
                         "# # # # # # # # #",
                         "# # # # # # # # #")
                 .addIngredient('#', border)
@@ -37,6 +32,7 @@ public class MywarpInventoryMenu implements BaseMenu {
                 .addIngredient('b', new ListMywarpMenuItem(this.connector))
                 .addIngredient('c', new DelMywarpMenuItem(this.connector))
                 .addIngredient('d', new SettingMywarpMenuItem(this.connector))
+                .addIngredient('e', new VisitMywarpMenuItem(this.connector))
                 .build();
 
         xyz.xenondevs.invui.window.Window window = Window.single()
