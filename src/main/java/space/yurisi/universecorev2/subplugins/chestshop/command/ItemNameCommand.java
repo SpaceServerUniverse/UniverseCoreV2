1b9e65b3e16baedd1027676d6f67c66fb6d56cb7
tatchan <43107628+TATCHAN0825@users.noreply.github.com>
2024-09-28 00:31:49 +0900

    アイテム名を小文字にする

--- src/main/java/space/yurisi/universecorev2/subplugins/chestshop/command/ItemNameCommand.java
+++ src/main/java/space/yurisi/universecorev2/subplugins/chestshop/command/ItemNameCommand.java
@@ -1,16 +1,22 @@
 package space.yurisi.universecorev2.subplugins.chestshop.command;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.translation.TranslationRegistry;
+import net.kyori.adventure.translation.Translator;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.jetbrains.annotations.NotNull;
 import space.yurisi.universecorev2.subplugins.chestshop.utils.SuperMessageHelper;
 
 public class ItemNameCommand implements CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
         if (!(sender instanceof Player)) {
             sender.sendMessage(SuperMessageHelper.getSuccessMessage("このコマンドはゲーム内で実行してください"));
             return false;
@@ -21,7 +27,7 @@ public class ItemNameCommand implements CommandExecutor {
             sender.sendMessage(SuperMessageHelper.getSuccessMessage("メインハンドに持ったアイテムの名前を表示します"));
             return false;
         } else {
-            sender.sendMessage(SuperMessageHelper.getSuccessMessage("§c" + item.getType()));
+            sender.sendMessage(SuperMessageHelper.getSuccessMessage("§a" + item.getType().name().toLowerCase()));
             return true;
         }
     }
