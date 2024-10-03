1030de4ce4e6ccbaa0d99be0e2d0e1b0aaa24deb
tatchan <43107628+TATCHAN0825@users.noreply.github.com>
2024-10-03 21:59:24 +0900

    未使用のimportの削除

--- src/main/java/space/yurisi/universecorev2/subplugins/tickfreezer/event/block/FadeEvent.java
+++ src/main/java/space/yurisi/universecorev2/subplugins/tickfreezer/event/block/FadeEvent.java
@@ -1,6 +1,5 @@
 package space.yurisi.universecorev2.subplugins.tickfreezer.event.block;
 
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFadeEvent;
