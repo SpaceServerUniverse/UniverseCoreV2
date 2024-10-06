8687ea976faa350d1520776f1f74ef0d76d1b631
ch4rindo <96crk.nk@gmail.com>
2024-10-06 15:18:57 +0900

    Add migration to remove duplicate land_permissions records
    - Delete duplicate land_id and uuid combinations, keeping one record

--- src/main/resources/db/migration/V2024.10.06__alter_table_land_permissions.sql
+++ src/main/resources/db/migration/V2024.10.06__alter_table_land_permissions.sql
@@ -1,3 +1,5 @@
 use SpaceServerUniverse;
 
+DELETE FROM land_permissions WHERE id NOT IN ( SELECT MIN(id) FROM land_permissions GROUP BY land_id, uuid) );
+
 ALTER TABLE land_permissions ADD UNIQUE(land_id, uuid);
