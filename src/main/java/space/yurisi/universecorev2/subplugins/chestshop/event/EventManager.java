070efcfe9328d5df7f026141e9c5c7e927870186
tatchan <43107628+TATCHAN0825@users.noreply.github.com>
2024-10-07 19:54:49 +0900

    土地保護内でチェストショップが買えない問題の修正

--- src/main/java/space/yurisi/universecorev2/subplugins/chestshop/event/EventManager.java
+++ src/main/java/space/yurisi/universecorev2/subplugins/chestshop/event/EventManager.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import space.yurisi.universecorev2.UniverseCoreV2;
 import space.yurisi.universecorev2.subplugins.chestshop.event.block.BreakEvent;
 import space.yurisi.universecorev2.subplugins.chestshop.event.block.SignChange;
+import space.yurisi.universecorev2.subplugins.chestshop.event.player.InteractEvent;
 
 public class EventManager {
     public EventManager(UniverseCoreV2 core){
@@ -12,5 +13,6 @@ public class EventManager {
     private void init(UniverseCoreV2 core){
         Bukkit.getPluginManager().registerEvents(new BreakEvent(),core);
         Bukkit.getPluginManager().registerEvents(new SignChange(),core);
+        Bukkit.getPluginManager().registerEvents(new InteractEvent(),core);
     }
 }
