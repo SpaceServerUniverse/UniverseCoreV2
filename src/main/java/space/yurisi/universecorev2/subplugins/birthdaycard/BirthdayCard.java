2ab8304bedf5760d079cf59027ee4cdc19cfd3ed
tatchan <43107628+TATCHAN0825@users.noreply.github.com>
2024-10-08 19:59:15 +0900

    基礎

---
+++ src/main/java/space/yurisi/universecorev2/subplugins/birthdaycard/BirthdayCard.java
@@ -0,0 +1,23 @@
+package space.yurisi.universecorev2.subplugins.birthdaycard;
+
+import space.yurisi.universecorev2.UniverseCoreV2;
+import space.yurisi.universecorev2.subplugins.SubPlugin;
+
+public class BirthdayCard implements SubPlugin {
+
+    public void onEnable(UniverseCoreV2 core) {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+    public String getName() {
+        return "BirthdayCard";
+    }
+
+    public String getVersion() {
+        return "1.0.0";
+    }
+}
---
+++ src/main/java/space/yurisi/universecorev2/subplugins/birthdaycard/event/player/JoinEvent.java
@@ -0,0 +1,7 @@
+package space.yurisi.universecorev2.subplugins.birthdaycard.event.player;
+
+import org.bukkit.event.Listener;
+
+public class JoinEvent implements Listener {
+
+}
