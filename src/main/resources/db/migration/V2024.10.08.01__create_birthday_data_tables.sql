2d5e9d9335b296c0bb627d3f1dc7dd11308622f8
tatchan <43107628+TATCHAN0825@users.noreply.github.com>
2024-10-08 23:38:30 +0900

    ;忘れてました

--- src/main/resources/db/migration/V2024.10.08.01__create_birthday_data_tables.sql
+++ src/main/resources/db/migration/V2024.10.08.01__create_birthday_data_tables.sql
@@ -1,3 +1,4 @@
+#完全に完成した時に完成した日にする
 use SpaceServerUniverse;
 
 CREATE TABLE IF NOT EXISTS birthday_datas
@@ -8,4 +9,4 @@ CREATE TABLE IF NOT EXISTS birthday_datas
     day        INT          NOT NULL,
     created_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     INDEX idx_month_day (month, day)
-)
+);
