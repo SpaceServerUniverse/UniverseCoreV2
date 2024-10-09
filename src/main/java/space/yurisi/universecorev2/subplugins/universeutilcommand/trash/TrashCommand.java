0188232850c98410878de64c25bee09d9997fba7
m1sk9 <me@m1sk9.dev>
2024-10-10 01:40:36 +0900

    feat: インベントリのスロット数を増やす

--- src/main/java/space/yurisi/universecorev2/subplugins/universeutilcommand/trash/TrashCommand.java
+++ src/main/java/space/yurisi/universecorev2/subplugins/universeutilcommand/trash/TrashCommand.java
@@ -6,6 +6,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import space.yurisi.universecorev2.utils.Message;
 
 public class TrashCommand implements CommandExecutor {
 
@@ -13,8 +14,10 @@ public class TrashCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
 
-        Inventory trashInv = player.getServer().createInventory(null, 9, Component.text("ゴミ箱 (取り扱い注意)"));
+        Inventory trashInv = player.getServer().createInventory(null, 54, Component.text("ゴミ箱 (取り扱い注意)"));
         player.openInventory(trashInv);
+
+        Message.sendSuccessMessage(player, "[ゴミ収集AI]", "ゴミ箱を開きました。捨てたアイテムは帰ってきません。取り扱いには十分注意してください。");
 
         return true ;
     }
