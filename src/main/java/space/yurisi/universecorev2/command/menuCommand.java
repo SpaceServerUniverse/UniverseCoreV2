c086fe70ef8cd6b8c8718c039d6221595c836128
m1sk9 <me@m1sk9.dev>
2024-10-11 18:26:55 +0900

    feat: メニューの追加

---
+++ src/main/java/space/yurisi/universecorev2/command/menuCommand.java
@@ -0,0 +1,37 @@
+package space.yurisi.universecorev2.command;
+
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import space.yurisi.universecorev2.menu.MainMenu;
+import space.yurisi.universecorev2.utils.Message;
+
+import java.util.Random;
+
+public class menuCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+        if (!(sender instanceof Player player)) {
+            return false;
+        }
+
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.sendMenu(player);
+
+        Random random = new Random();
+        if (random.nextInt(100) < 5) {
+            player.playSound(player.getLocation(), Sound.ENTITY_CREEPER_PRIMED, 10, 1);
+            player.playSound(player.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 10, 1);
+            Message.sendSuccessMessage(player, "[管理AI]", "§c§oメニューを開きました...ヮ! クリーパーが... (´・ω・`)");
+        } else {
+            player.playSound(player.getLocation(), Sound.BLOCK_AMETHYST_BLOCK_FALL, 10, 1);
+            Message.sendSuccessMessage(player, "[管理AI]", "メニューを開きました");
+        }
+
+        return true;
+    }
+}
