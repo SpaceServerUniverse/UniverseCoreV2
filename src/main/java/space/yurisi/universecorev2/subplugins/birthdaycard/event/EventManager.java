2e6b295445cef0d5743bd70a7aa18baaca5f26f7
tatchan <43107628+TATCHAN0825@users.noreply.github.com>
2024-10-12 16:13:54 +0900

    ぬるぽ

--- src/main/java/space/yurisi/universecorev2/subplugins/birthdaycard/event/EventManager.java
+++ src/main/java/space/yurisi/universecorev2/subplugins/birthdaycard/event/EventManager.java
@@ -1,20 +1,15 @@
 package space.yurisi.universecorev2.subplugins.birthdaycard.event;
 
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
-import org.jetbrains.annotations.NotNull;
 import space.yurisi.universecorev2.UniverseCoreV2;
 import space.yurisi.universecorev2.subplugins.birthdaycard.event.player.JoinEvent;
 
 public class EventManager {
-    private UniverseCoreV2 core;
-
     public EventManager(UniverseCoreV2 core) {
-        init();
+        init(core);
     }
 
-    private void init() {
-
+    private void init(UniverseCoreV2 core) {
         Bukkit.getPluginManager().registerEvents(new JoinEvent(), core);
     }
 }
