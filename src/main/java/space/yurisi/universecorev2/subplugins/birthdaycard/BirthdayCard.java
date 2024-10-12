71ad1e5b7a774961967fb12693d2b58d984f891f
tatchan <43107628+TATCHAN0825@users.noreply.github.com>
2024-10-12 16:07:56 +0900

    なんか動かないって思ってたらnew忘れてた

--- src/main/java/space/yurisi/universecorev2/subplugins/birthdaycard/BirthdayCard.java
+++ src/main/java/space/yurisi/universecorev2/subplugins/birthdaycard/BirthdayCard.java
@@ -3,12 +3,14 @@ package space.yurisi.universecorev2.subplugins.birthdaycard;
 import space.yurisi.universecorev2.UniverseCoreV2;
 import space.yurisi.universecorev2.subplugins.SubPlugin;
 import space.yurisi.universecorev2.subplugins.birthdaycard.command.BirthdayCardCommand;
+import space.yurisi.universecorev2.subplugins.birthdaycard.event.EventManager;
 
 public class BirthdayCard implements SubPlugin {
     public static String PREFIX = "[お誕生日AI]";
 
     public void onEnable(UniverseCoreV2 core) {
         core.getCommand("birthday").setExecutor(new BirthdayCardCommand());
+        new EventManager(core);
     }
 
     public void onDisable() {
