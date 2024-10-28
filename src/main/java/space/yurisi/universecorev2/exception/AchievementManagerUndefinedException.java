4344e36e71b872441fb613785cf35decc757cfc8
syoyu <ryasyo1224@gmail.com>
2024-10-29 00:18:34 +0900

    なぜのexception

--- src/main/java/space/yurisi/universecorev2/exception/AchievementManagerUndefinedException.java
+++
@@ -1,7 +0,0 @@
-package space.yurisi.universecorev2.exception;
-
-public class AchievementManagerUndefinedException extends RuntimeException {
-    public AchievementManagerUndefinedException(String message) {
-        super(message);
-    }
-}
