8028fdbb3cb50ef1c56c399a504951c26573f613
syoyu <ryasyo1224@gmail.com>
2024-11-04 22:25:27 +0900

    ちゃんと動作確認した？マジで

--- src/main/java/space/yurisi/universecorev2/subplugins/achievement/menu/ProgressMenu.java
+++ src/main/java/space/yurisi/universecorev2/subplugins/achievement/menu/ProgressMenu.java
@@ -52,7 +52,7 @@ public class ProgressMenu implements BaseMenu{
             Long currentGoal = data.getCurrentGoal();
             int cell = (int) (currentGoal / 7);
             int achievedCells = (int) (currentCount / cell);
-            builder = builder.addContent(new ProgressBarItem((i % 7) <= achievedCells, data.getStage(), data.getItemName()));
+            builder = builder.addContent(new ProgressBarItem((i % 8) <= achievedCells, data.getStage(), data.getItemName()));
         }
 
         Window window = Window.single()
