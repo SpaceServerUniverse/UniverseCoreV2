ee90080ae7d8ed8a12d9ef62ba20f07c2fff0bb8
syoyu <ryasyo1224@gmail.com>
2024-11-09 01:27:38 +0900

    NMS追加できた。。。

--- src/main/java/space/yurisi/universecorev2/subplugins/dekakinanticheat/protocol/adapter/ReceivePacketAdapter.java
+++ src/main/java/space/yurisi/universecorev2/subplugins/dekakinanticheat/protocol/adapter/ReceivePacketAdapter.java
@@ -4,7 +4,9 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
+import net.minecraft.network.protocol.game.ServerboundPlayerAbilitiesPacket;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import space.yurisi.universecorev2.UniverseCoreV2;
 import space.yurisi.universecorev2.subplugins.dekakinanticheat.protocol.ReceivePacketTypes;
@@ -17,10 +19,12 @@ public class ReceivePacketAdapter extends PacketAdapter {
 
     @Override
     public void onPacketReceiving(PacketEvent event) {
-        analyzePacket(event.getPacket());
+        analyzePacket(event.getPacket(), event.getPlayer());
     }
 
-    public void analyzePacket(PacketContainer container){
-        Bukkit.getLogger().info(container.getType().toString());
+    public void analyzePacket(PacketContainer container, Player player){
+        if(container.getHandle() instanceof ServerboundPlayerAbilitiesPacket pk){
+
+        }
     }
 }
