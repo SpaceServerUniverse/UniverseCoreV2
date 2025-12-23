0dfe63b3eefb11c0e794e686692eb1db769095dc
meyason <nori.tobimori@icloud.com>
2025-12-23 23:05:01 +0900

    fix untilized

--- src/main/java/space/yurisi/universecorev2/utils/material_type/Armor.java
+++ src/main/java/space/yurisi/universecorev2/utils/material_type/Armor.java
@@ -6,30 +6,37 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Armor {
+
     public static final Material[] armors = {
-            Material.IRON_BOOTS,
-            Material.DIAMOND_BOOTS,
+
             Material.LEATHER_BOOTS,
-            Material.NETHERITE_BOOTS,
+            Material.IRON_BOOTS,
             Material.GOLDEN_BOOTS,
+            Material.DIAMOND_BOOTS,
             Material.CHAINMAIL_BOOTS,
-            Material.DIAMOND_LEGGINGS,
-            Material.GOLDEN_LEGGINGS,
+            Material.NETHERITE_BOOTS,
+
             Material.LEATHER_LEGGINGS,
             Material.IRON_LEGGINGS,
-            Material.NETHERITE_LEGGINGS,
+            Material.GOLDEN_LEGGINGS,
+            Material.DIAMOND_LEGGINGS,
             Material.CHAINMAIL_LEGGINGS,
-            Material.IRON_CHESTPLATE,
+            Material.NETHERITE_LEGGINGS,
+
             Material.LEATHER_CHESTPLATE,
-            Material.DIAMOND_CHESTPLATE,
+            Material.IRON_CHESTPLATE,
             Material.GOLDEN_CHESTPLATE,
+            Material.DIAMOND_CHESTPLATE,
             Material.CHAINMAIL_CHESTPLATE,
-            Material.LEATHER_HELMET,
+            Material.NETHERITE_CHESTPLATE,
+
             Material.TURTLE_HELMET,
+            Material.LEATHER_HELMET,
             Material.IRON_HELMET,
-            Material.DIAMOND_HELMET,
             Material.GOLDEN_HELMET,
-            Material.CHAINMAIL_HELMET
+            Material.DIAMOND_HELMET,
+            Material.CHAINMAIL_HELMET,
+            Material.NETHERITE_HELMET
     };
 
     public static boolean isArmor(Material material){
