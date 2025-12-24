4699596ed5698b40391e372fc072c7906e8f351c
yuyaprgrm <yuyaprgrm@outlook.jp>
2025-12-24 19:27:26 +0900

    tidy

--- src/main/java/space/yurisi/universecorev2/subplugins/chestshop/transaction/action/RollbackFunc.java
+++ src/main/java/space/yurisi/universecorev2/subplugins/chestshop/transaction/action/RollbackFunc.java
@@ -1,6 +1,5 @@
 package space.yurisi.universecorev2.subplugins.chestshop.transaction.action;
 
-import org.jetbrains.annotations.Nullable;
 import space.yurisi.universecorev2.subplugins.chestshop.transaction.TransactionException;
 
 public interface RollbackFunc {
