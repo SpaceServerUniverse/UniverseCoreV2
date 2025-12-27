34208685724506781cdecbb03195babd4407d31e
yuyaprgrm <yuyaprgrm@outlook.jp>
2025-12-27 12:20:41 +0900

    Make RollbackFunc @FunctionalInterface

--- src/main/java/space/yurisi/universecorev2/subplugins/chestshop/transaction/action/RollbackFunc.java
+++ src/main/java/space/yurisi/universecorev2/subplugins/chestshop/transaction/action/RollbackFunc.java
@@ -2,6 +2,7 @@ package space.yurisi.universecorev2.subplugins.chestshop.transaction.action;
 
 import space.yurisi.universecorev2.subplugins.chestshop.transaction.TransactionException;
 
+@FunctionalInterface
 public interface RollbackFunc {
     void execute() throws TransactionException;
 }
