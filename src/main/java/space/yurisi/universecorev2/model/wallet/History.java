0daa48f16d48e5c9c443ff000559c11e1eae2531
yuyaprgrm <yuyaprgrm@outlook.jp>
2025-12-31 01:27:50 +0900

    consistency

--- src/main/java/space/yurisi/universecorev2/model/wallet/History.java
+++ src/main/java/space/yurisi/universecorev2/model/wallet/History.java
@@ -1,14 +1,14 @@
 package space.yurisi.universecorev2.model.wallet;
 
 import org.jetbrains.annotations.NotNull;
+import space.yurisi.universecorev2.model.user.UserId;
 
 import java.util.Date;
-import java.util.UUID;
 
 public record History(
-        @NotNull UUID userId,
+        @NotNull UserId userId,
         @NotNull Long delta,
         @NotNull String reason,
-        @NotNull Date createdAt
+        @NotNull Date timestamp
 ) {
 }
