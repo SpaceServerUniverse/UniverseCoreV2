53aeee5b3285d124a0d81b18152599516bd29167
dev_kimchi <kimchimail@protonmail.com>
2026-01-24 21:17:28 +0900

    クリーパーやTNTの爆発によって防具立てが破壊される問題を修正

--- src/main/java/space/yurisi/universecorev2/event/entity/EntityHangingBreakEvent.java
+++ src/main/java/space/yurisi/universecorev2/event/entity/EntityHangingBreakEvent.java
@@ -1,8 +1,10 @@
 package space.yurisi.universecorev2.event.entity;
 
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.hanging.HangingBreakByEntityEvent;
 import org.bukkit.event.hanging.HangingBreakEvent;
 
@@ -11,6 +13,16 @@ public class EntityHangingBreakEvent implements Listener {
     public void onHanging(HangingBreakByEntityEvent event) {
         if(event.getCause() == HangingBreakEvent.RemoveCause.EXPLOSION){
             event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onDamage(EntityDamageEvent event) {
+        if (event.getEntityType() == EntityType.ARMOR_STAND) {
+            if (event.getCause() == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION ||
+                    event.getCause() == EntityDamageEvent.DamageCause.BLOCK_EXPLOSION) {
+                event.setCancelled(true);
+            }
         }
     }
 }
