8dd44ba88d921b0d35731c741d4f9716d8dba4cd
dev_kimchi <kimchimail@protonmail.com>
2026-01-25 13:56:08 +0900

    Flyコマンドの実装(初期はOP以外入力不可)

--- src/main/java/space/yurisi/universecorev2/subplugins/flysystem/command/FlyCommand.java
+++ src/main/java/space/yurisi/universecorev2/subplugins/flysystem/command/FlyCommand.java
@@ -5,44 +5,48 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import java.util.Locale;
 
-public class FlyCommand implements CommandExecutor {
+public class    FlyCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         // 実行者がプレイヤーか確認　サーバーなどプレイヤー以外ならエラーを出す
         if (!(sender instanceof Player player)) {
             sender.sendMessage("このコマンドはゲーム内から実行してください。");
-            return true;
+            return false;
         }
 
         // 権限を確認 (fly という権限を持っているか)
         // OPは最初から持っているが、Luckparmsで他のプレイヤーにも付与も可能
         if (!player.hasPermission("fly")) {
             player.sendMessage("§cこのコマンドを実行する権限がありません。");
-            return true;
+            return false;
         }
 
         // /fly のみだった場合 (onやoffなどの文字が入っていない場合)
         if (args.length == 0) {
             player.sendMessage("§c使い方が正しくありません: /fly <on|off>");
-            return true;
+            return false;
         }
 
         // flymodeという変数を作ってそのコマンドを全て小文字にして返す
-        String flymode = args[0].toLowerCase();
+        String flyMode = args[0].toLowerCase(Locale.ROOT);
 
-        if (flymode.equals("on")) {
-            player.setAllowFlight(true);
-            player.sendMessage("§a飛行モードを有効にしました。");
-        } else if (flymode.equals("off")) {
-            player.setAllowFlight(false);
-            player.setFlying(false); // 飛行中にoffにした場合、その場で落下させる
-            player.sendMessage("§a飛行モードを無効にしました。");
-        } else {
-            player.sendMessage("§cコマンドは on または off を指定してください。");
+        switch (flyMode) {
+            case "on" -> {
+                player.setAllowFlight(true);
+                player.sendMessage("§a飛行モードを有効にしました。");
+            }
+            case "off" -> {
+                player.setAllowFlight(false);
+                player.setFlying(false); // 飛行中にoffにした場合、その場で落下
+                player.sendMessage("§a飛行モードを無効にしました。");
+            }
+            default -> {
+                player.sendMessage("§cコマンドは on または off を指定してください。");
+            }
         }
-
         return true;
     }
 }
