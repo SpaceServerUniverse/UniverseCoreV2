3628a6420157435a2f6c0cb734c2795e2858195e
Sho Sakuma <me@m1sk9.dev>
2026-01-31 13:57:43 +0900

    古い実装を削除

--- src/main/java/space/yurisi/universecorev2/subplugins/universediscord/exception/DiscordChannelNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package space.yurisi.universecorev2.subplugins.universediscord.exception;
-
-public class DiscordChannelNotFoundException extends RuntimeException {
-    public DiscordChannelNotFoundException(String message) {
-        super(message);
-    }
-}
